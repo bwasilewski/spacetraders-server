cef5bbdbc9c98dd0587488ac066a8a17a0fcac6f
Benjamin Wasilewski <benwasilewski@gmail.com>
2024-01-22 19:27:03 -0600

    factions, waypoints, contracts, fleet, market

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import fetch from 'node-fetch'
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/api/player', async ({res}) => {
   const response = await fetch('https://api.spacetraders.io/v2/my/agent', {
@@ -17,9 +20,33 @@ app.get('/api/player', async ({res}) => {
   return res.json(data)
 })
 
-app.get('/api/location', async (req, res) => {
+app.get('/api/system/:symbol', async (req, res) => {
+  const { symbol } = req.params
+  const response = await fetch(`https://api.spacetraders.io/v2/systems/${symbol}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/system/:symbol/waypoints', async (req, res) => {
+  const { symbol } = req.params
+  const { limit = 10, page = 1 } = req.query
+  const response = await fetch(`https://api.spacetraders.io/v2/systems/${symbol}/waypoints?limit=${limit}&page=${page}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/system/:system/waypoint/:waypoint', async (req, res) => {
   console.log('Fetching location....')
-  const { system, waypoint } = req.query
+  console.log(req.params)
+  const { system, waypoint } = req.params
   const response = await fetch(
     `https://api.spacetraders.io/v2/systems/${system}/waypoints/${waypoint}`, {
     headers: {
@@ -27,21 +54,101 @@ app.get('/api/location', async (req, res) => {
     }
   })
   const data = await response.json()
-  console.log('Location Data: ', data)
   return res.json(data)
 })
 
-app.get('/api/contracts', async (req, res) => {
-  console.log('Fetching contracts....')
+
+app.get('/api/system/:system/waypoint/:waypoint/market', async (req, res) => {
+  const { system, waypoint } = req.params
+
+  const response = await fetch(`https://api.spacetraders.io/v2/systems/${system}/waypoints/${waypoint}/market`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/systems', async (req, res) => {
+  const { limit = 10, page = 1 } = req.query
+  const response = await fetch(`https://api.spacetraders.io/v2/systems?page=${page}&limit=${limit}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/fleet', async (req, res) => {
+  const response = await fetch(`https://api.spacetraders.io/v2/my/ships`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/fleet/:symbol', async (req, res) => {
+  const { symbol } = req.params
+  const response = await fetch(`https://api.spacetraders.io/v2/my/ships/${symbol}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/contracts/list', async (req, res) => {
   const response = await fetch(`https://api.spacetraders.io/v2/my/contracts`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+app.get('/api/contract/:id', async (req, res) => {
+  const { id } = req.params
+  const response = await fetch(`https://api.spacetraders.io/v2/my/contracts/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${process.env.TOKEN}`
+    }
+  })
+  const data = await response.json()
+  return res.json(data)
+})
+
+
+// app.get('/api/contracts/negotiate', async (req, res) => {
+//   const { shipSymbol } = req.query
+//   const response = await fetch(`https://api.spacetraders.io/v2/my/ships/${shipSymbol}/negotiate/contract`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${process.env.TOKEN}`
+//     },
+//   })
+//   const data = await response.json()
+//   return res.json(data)
+// })
+
+app.get('/api/factions', async (req, res) => {
+  const { page } = req.query
+  console.log('Requested Page: ', page)
+  const response = await fetch(`https://api.spacetraders.io/v2/factions?page=${page}`, {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${process.env.TOKEN}`
     }
   })
   const data = await response.json()
-  console.log('Response Data: ', data)
   return res.json(data)
 })
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
